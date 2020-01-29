da7c0dce620134cdcfe85c68e6a193d67b5fac54
hkoops22 <59522465+hkoops22@users.noreply.github.com>
2020-01-29 16:10:01 -0700

    Added JS menu for mobile

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("Hi, it's Hannah!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle')
+  //searches the doc for the ID myNavtoggle in the div
+  //assigned nav object new variable x
+  if (x. className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+  //we want to show the menu if it isn't shown or viceversa so if/then statement added
+  //browser checks classname of x to see if it's navtoggle or not, if it is then adds responsive if not it will change className back aka hide the menu
+  
+
+}
